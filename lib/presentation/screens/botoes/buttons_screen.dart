ca1d8f76bc5d9803c5eaa2e363b9da1299f00323
Eduardo Cintra dos Santos <eduardocintradossantos@gmail.com>
2025-03-17 14:25:42 -0300

    Criado cartões

--- lib/presentation/screens/botoes/buttons_screen.dart
+++ lib/presentation/screens/botoes/buttons_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class ButtonsScreen extends StatelessWidget {
   const ButtonsScreen({super.key});
@@ -8,6 +9,53 @@ class ButtonsScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Tela de Botões'),
+      ),
+      body: _BottonsView(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){context.pop();},
+        child: Icon(Icons.arrow_back_ios_new_rounded),
+      ),
+    );
+  }
+}
+
+class _BottonsView extends StatelessWidget {
+  const _BottonsView();
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+        child: Wrap(
+          spacing: 10,
+          alignment: WrapAlignment.center,
+          children: [
+            ElevatedButton(onPressed: (){}, child: const Text('Elevated')),
+            ElevatedButton(onPressed: null, child: const Text('Elevated Desbled')),
+            ElevatedButton.icon(onPressed: (){}, 
+            icon: const Icon(Icons.access_alarm_rounded),
+            label: const Text('Elevated Icon')),
+
+            FilledButton(onPressed: (){}, child: const Text('Filled')),
+            FilledButton.icon(
+              onPressed: (){}, 
+              icon: const Icon(Icons.accessibility_new),
+              label: Text('Filled Icon'),),
+
+            OutlinedButton(onPressed: (){}, child: const Text('Outlined')),
+            OutlinedButton.icon(
+              onPressed: (){}, 
+              icon: const Icon(Icons.terminal),
+              label: Text('Outlined Icon')),
+
+            TextButton(onPressed: (){}, child: const Text('Text')),
+            TextButton.icon(onPressed: (){}, 
+            icon: Icon(Icons.account_box_outlined),
+            label: Text('Text Icon'),)
+          ],
+        ),  
       ),
     );
   }
