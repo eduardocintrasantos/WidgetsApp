fc839aaee1ebbd76727f0fcc882d96c87c63c27f
Eduardo Cintra dos Santos <eduardocintradossantos@gmail.com>
2025-11-04 13:01:41 -0300

    Navegação com go_router

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_itens.dart';
 import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 
@@ -55,7 +56,7 @@ class _CustomListTitle extends StatelessWidget {
             builder: (context) => const ButtonsScreen() 
           )
         );*/
-        Navigator.pushNamed(context, menuItem.link);
+        context.push(menuItem.link);
       },
     );
   }
